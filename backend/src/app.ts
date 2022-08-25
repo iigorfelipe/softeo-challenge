7c84dbfa37cb1ce89fa0fa3265c5baa38b54e89f
Igor <iigorfelipe@gmail.com>
2022-08-25 17:16:23 -0300

    ✨ feat: import the routes into class App and create the routes method in it

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,19 @@
 import express, { Express } from 'express';
 import db from './database/db';
+import router from './routes';
 
 class App {
   public app: Express;
 
   constructor() {
     this.app = express();
+    this.routes();
   }
+
+  private routes = () => {
+    this.app.use(express.json());
+    this.app.use(router);
+  };
 
   public start = (PORT: string | number): void => {
     this.app.listen(PORT, async () => {
