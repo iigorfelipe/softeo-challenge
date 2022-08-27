524aec610a395452ea32f85be963c4138c570171
Igor <iigorfelipe@gmail.com>
2022-08-27 17:34:38 -0300

    🎨 style: stylizes the Header component

---
+++ frontend/src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 5px 0px 5px;
+`;
+
+export const Details = styled.header`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Values = styled.header`
+  display: flex;
+  flex-direction: column;
+`;
