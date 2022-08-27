cccf6be15b0d20c67ad948d41c1dd7808ed3f725
Igor <iigorfelipe@gmail.com>
2022-08-27 17:36:49 -0300

    ✨ feat: create a Main page to encapsulate the components

---
+++ frontend/src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import Users from '../../components/Users';
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Users/>
+    </div>
+  )
+};
+
+export default Main;
