e43caaf0b8de571ab0e1f6491d63d944ef6b9503
Igor <iigorfelipe@gmail.com>
2022-08-27 17:39:03 -0300

    ✨ feat: imports the Main page and renders it in the App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import { theme } from './styles/theme';
+import Main from './pages/Main';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      gomu gomu no...
+      <Main />
     </ThemeProvider>
   );
 }
