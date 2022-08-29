03e2673af487bbcf41f600475b02aaec0629787f
Igor <iigorfelipe@gmail.com>
2022-08-29 15:43:52 -0300

    ✨ feat: add an avatar icon to the User component

--- frontend/src/components/Users/index.tsx
+++ frontend/src/components/Users/index.tsx
@@ -2,20 +2,30 @@ import * as C from './styles';
 import { UsersType } from '../../types/UsersType';
 import { useState } from 'react';
 import getAllUser from '../../api/getAllUsers';
+import { Icon } from '@iconify/react';
 
 const Users = () => {
   const [users, setUsers] = useState<UsersType[]>();
   
   getAllUser().then((response) => setUsers(response.data));
-
   return (
     <C.Container>
       {
         users &&
         users.map((item) => (
           <C.Users key={item.id}>
-            <C.User>{item.name}</C.User>
+
+            <C.User>
+              <Icon
+                icon='healthicons:ui-user-profile-outline'
+                width='45'
+                height='45'
+                />
+                {item.name}
+            </C.User>
+
             <C.User>{item.value}</C.User>
+
           </C.Users>
         ))
       }
