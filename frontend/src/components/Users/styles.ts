cc8492763c9bf1ea8d7dc8bab6a05b458981f743
Igor <iigorfelipe@gmail.com>
2022-08-29 15:45:44 -0300

    🎨 styles: stylizes the User component

--- frontend/src/components/Users/styles.ts
+++ frontend/src/components/Users/styles.ts
@@ -2,16 +2,22 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 80vh;
+  border: 1px solid gray;
   margin-bottom: 20px;
 `;
 
 export const Users = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-bottom: 10px;
-  padding: 0px 5px;
+  align-items: center;
+  margin: 10px;
+  padding: 2px 5px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  font-size: large;
 `;
 
 export const User = styled.div`
-  /* border: 1px solid red; */
+  display: flex;
+  align-items: center;
 `;
