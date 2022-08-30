7e9c5d578385d88b644a42076aa47f2362e3b08d
Igor <iigorfelipe@gmail.com>
2022-08-29 22:34:28 -0300

    ✨ feat: provides the routes for the entire application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,18 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import { theme } from './styles/theme';
 import Main from './pages/Main';
+import AppRoutes from './routes';
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <ThemeProvider theme={theme}>
+  
       <GlobalStyle />
-      <Main />
+
+        <AppRoutes>
+          <Main />
+        </AppRoutes>
+  
     </ThemeProvider>
   );
 }
