6534b0e425cd08ddf242dc9a25d55135a3f35f99
Igor <iigorfelipe@gmail.com>
2022-08-30 00:05:43 -0300

    ✨ feat: create a UseProvider

---
+++ frontend/src/contexts/UserProvider.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useState } from 'react';
+import { UsersType } from '../types/UsersType';
+import UserContext from './UserContext';
+import getAllUser from '../api/getAllUsers';
+
+type UserContextProps = {
+  children: ReactNode;
+};
+
+const UserContextProvider = ({children}: UserContextProps) => {
+  const [users, setUsers] = useState<UsersType[]>([]);
+
+  getAllUser().then((response) => setUsers(response.data));;
+
+  return (
+    <UserContext.Provider value={{
+        users,
+        setUsers,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+};
+
+export default UserContextProvider;
