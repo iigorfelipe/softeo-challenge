df25be5b9af4be939cd1c38d1a3bacb4bc497f97
Igor <iigorfelipe@gmail.com>
2022-08-31 11:50:47 -0300

    ✨ feat: create function to format a value in a string with the BRL currency format

---
+++ frontend/src/utils/formatToCurrencies.ts
@@ -0,0 +1,9 @@
+export const formatToBRL = (value: number): string => {
+  const price =  new Intl.NumberFormat('pt-br', {
+    currency: 'BRL',
+    style: 'currency',
+    minimumFractionDigits: 2,
+  });
+
+  return price.format(+value)
+}
