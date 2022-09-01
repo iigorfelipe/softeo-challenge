78c260a7279b0eee9e0a6c849ba907a6383d79a2
Igor <iigorfelipe@gmail.com>
2022-09-01 16:02:46 -0300

    ✨ feat: implements the UserDetailsHeader component

--- frontend/src/components/UserDetails/Header/index.tsx
+++ frontend/src/components/UserDetails/Header/index.tsx
@@ -1,10 +1,19 @@
 import { UserProps } from '../../../types/UsersType';
-import * as C from '../styles';
+import { Icon } from '@iconify/react';
+import * as C from './styles';
 
-const UserDetailsHeader = ({ user }: UserProps) => {
+const UserDetailsHeader = ({ user: { name } }: UserProps): JSX.Element => {
   return (
     <C.Header>
-      avatar e name
+
+      <Icon
+        icon='healthicons:ui-user-profile-outline'
+        width='45'
+        height='45'
+      />
+
+      <C.Name>{name}</C.Name>
+      <></>
     </C.Header>
   )
 };
