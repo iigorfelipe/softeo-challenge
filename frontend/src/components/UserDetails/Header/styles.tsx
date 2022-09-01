e09bc6531de5636645426f1e4dcfcef56924b6d1
Igor <iigorfelipe@gmail.com>
2022-09-01 16:03:31 -0300

    🎨 style: stylizes the UserDetailHeader component

---
+++ frontend/src/components/UserDetails/Header/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  border: 1px solid gray;
+  border-radius: 15px;
+  margin: 5px 25px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const Name = styled.div`
+  font-size: large;
+`;
