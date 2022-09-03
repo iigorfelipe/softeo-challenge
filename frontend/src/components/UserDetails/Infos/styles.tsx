21fb468d95509f01bb1ef1e6c76e04303093a7db
Igor <iigorfelipe@gmail.com>
2022-09-02 21:02:40 -0300

    🎨 style: styling the UserDetailsInfos component

---
+++ frontend/src/components/UserDetails/Infos/styles.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Infos = styled.div`
+  font-size: 1.1em;
+  border: 1px solid gray;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  margin: 2px;
+  padding: 2px;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Contanct = styled.div`
+  margin: 5px 0px;
+  border-bottom: 1px solid gray;
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 3px;
+`;
+
+export const Details = styled.div`
+  font-size: 1.1em;
+  border: 1px solid gray;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  margin: 2px;
+  padding: 2px;
+  align-items: center;
+  width: 60%;
+`;
+
+export const Values = styled.div`
+  margin: 10px 0px;
+  border-bottom: 1px solid gray;
+`;
