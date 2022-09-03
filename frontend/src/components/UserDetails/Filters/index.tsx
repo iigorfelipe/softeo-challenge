3e8795b753e648afd7a9a6c767f949db4ba2da11
Igor <iigorfelipe@gmail.com>
2022-09-03 12:35:56 -0300

    ✨ feat: implements the UserDetailsFilter component

--- frontend/src/components/UserDetails/Filters/index.tsx
+++ frontend/src/components/UserDetails/Filters/index.tsx
@@ -1,23 +1,63 @@
+import { divideOfValues } from '../../../helpers/calculateValues';
+import { formatToBRL } from '../../../helpers/formatToCurrencies';
 import { UserProps } from '../../../types/UsersType';
-import * as C from '../styles';
+import * as C from './styles';
 
 const UserDetailsFilter = ({ user }: UserProps) => {
+  const value = formatToBRL(+user.value / user.parcel);
+
   return (
-    <C.FilterContainer>
+    <C.Container>
 
       <C.Message>
-        mensagem
+        {`Neste mês você receberá ${value} de ${user.name}`}
       </C.Message>
 
-      <C.Filter>3 meses</C.Filter>
-      <C.Filter>6 meses</C.Filter>
-      <C.Filter>1 meses</C.Filter>
+      <C.P>Verifique outros períodos:</C.P>
 
-      <C.CustomFilter>
-        Personalizado
-      </C.CustomFilter>
+      <C.FilterContainer>
 
-    </C.FilterContainer>
+        <C.Filters>
+
+          <C.Filter>
+
+            <C.Border>
+              <C.Number>3</C.Number>
+            </C.Border>
+
+            <C.Data>meses</C.Data>
+
+          </C.Filter>
+          
+          <C.Filter>
+
+            <C.Border>
+              <C.Number>6</C.Number>
+            </C.Border>
+
+            <C.Data>meses</C.Data>
+
+          </C.Filter>
+          
+          <C.Filter>
+
+            <C.Border>
+              <C.Number>1</C.Number>
+            </C.Border>
+
+            <C.Data>ano</C.Data>
+
+          </C.Filter>
+
+        </C.Filters>
+
+        <C.CustomFilter>
+          Escolha um outro período
+        </C.CustomFilter>
+      
+      </C.FilterContainer>
+
+    </C.Container>
   )
 };
 
