d9fc5fcf621d155d2a46eebdd3ec6d87cef8cf77
Igor <iigorfelipe@gmail.com>
2022-09-02 21:01:46 -0300

    ✨ feat: implements the UserDetailsInfos component

--- frontend/src/components/UserDetails/Infos/index.tsx
+++ frontend/src/components/UserDetails/Infos/index.tsx
@@ -1,16 +1,54 @@
 import { UserProps } from '../../../types/UsersType';
-import * as C from '../styles';
+import { Icon } from '@iconify/react';
+import * as C from './styles';
+import { formatToBRL } from '../../../helpers/formatToCurrencies';
 
 const UserDetailsInfos = ({ user }: UserProps): JSX.Element => {
+
+  const temporaryFakeContact = {
+    whatsapp: '(00) 0 0000-0000',
+    email: 'email@email.com',
+  };
+  
   return (
     <C.Container>
 
       <C.Infos>
-        informaçãoes do usuário
+
+        <C.Contanct>{`${user.name}'s Contacts`}</C.Contanct>
+
+        <C.Contanct>
+
+          <C.Icon>
+            <Icon
+              icon='akar-icons:whatsapp-fill'
+              width='15'
+            />
+          </C.Icon>
+
+          {temporaryFakeContact.whatsapp}
+        </C.Contanct>
+
+        <C.Contanct>
+
+          <C.Icon>
+            <Icon
+              icon='carbon:email'
+              width='17'
+            />
+          </C.Icon>
+
+          {temporaryFakeContact.email}
+        </C.Contanct>
+
       </C.Infos>
 
       <C.Details>
-        valores
+
+        <C.Values>{formatToBRL(+user.value)}</C.Values>
+        <C.Values>{`Parcels: ${user.parcel}`}</C.Values>
+        <C.Values>{`Payday: ${user.payday}`}</C.Values>
+  
       </C.Details>
   
     </C.Container>
