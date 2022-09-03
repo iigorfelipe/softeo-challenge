f2bc804204a1f789d269740dc04a23b3d8ea228c
Igor <iigorfelipe@gmail.com>
2022-09-03 12:36:55 -0300

    🎨 style: styles the UserDetailsFilter component

---
+++ frontend/src/components/UserDetails/Filters/styles.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Message = styled.div`
+  margin: 15px 3px;
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid blueviolet;
+  border-radius: 5px;
+`;
+
+export const FilterContainer = styled.div`
+  border: 1px solid blueviolet;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 5px;
+`;
+
+export const P = styled.p`
+  margin: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Filters = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const Filter = styled.div`
+  border: 1px solid blueviolet;
+  border-radius: 5px;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Border = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 25px;
+  margin: 5px;
+  border: 1px solid blueviolet;
+  border-radius: 50%;
+`;
+
+export const Number = styled.div`
+  font-size: 2.3em;
+`;
+
+export const Data = styled.p`
+  border-bottom: 1px solid blueviolet;
+`;
+
+export const CustomFilter = styled.div`
+  border: 1px solid blueviolet;
+  border-radius: 5px;
+  margin: 15px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2em;
+`;
+
+export const ButtonCustomFilter = styled.div`
+`;
+
