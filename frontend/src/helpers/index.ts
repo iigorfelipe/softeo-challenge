77b96bf39715ce14d41c4ba9a59b6250ab0df907
Igor <iigorfelipe@gmail.com>
2022-09-06 19:43:01 -0300

    ✨ feat: creates an index to store all the helpers functions

---
+++ frontend/src/helpers/index.ts
@@ -0,0 +1,13 @@
+import { divideOfValues, sumOfValues, sumOfValuesFormatted } from './calculateValues';
+import { formatDateToBRL } from './formatDates';
+import { formatToBRL } from './formatToCurrencies';
+
+export {
+  divideOfValues,
+  sumOfValues,
+  sumOfValuesFormatted,
+  formatDateToBRL,
+  formatToBRL,
+}
+
+// use these functions by importing '* as H';
