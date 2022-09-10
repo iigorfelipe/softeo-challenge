4041590d668a3f43ddc845dcc418101b74980806
Igor <iigorfelipe@gmail.com>
2022-09-10 01:07:57 -0300

    ✨ feat: implements functionality to fetch the value of a custom period

--- frontend/src/components/UserDetails/Filters/index.tsx
+++ frontend/src/components/UserDetails/Filters/index.tsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
-import { formatToBRL } from '../../../helpers/formatToCurrencies';
 import { UserProps } from '../../../types/UsersType';
 import swal from 'sweetalert';
+import * as H from '../../../helpers'
 import * as C from './styles';
+import { Icon } from '@iconify/react';
 
-const UserDetailsFilter = ({ user }: UserProps) => {
+const UserDetailsFilter = ({ user }: UserProps): JSX.Element => {
   const initialValue = +user.value / user.parcel;
-  
+
   const [value, setValue] = useState(initialValue);
-  const [period, setPeriod] = useState('mês');
+  const [textPeriod, setTextPeriod] = useState('mês');
+  const [customDate, setCustomDate] = useState<string>('');
+
   const [option1] = useState(3);
   const [option2] = useState(6);
   const [option3] = useState(1);
 
-  const totalValueOfMonth = formatToBRL(value);
+  const totalValueOfMonth = H.formatToBRL(value);
+
+  const [periodValue, setPeriodValue] = useState(totalValueOfMonth);
 
   const calculate = (option: number) => {
     if (option === option3) option = 12;
@@ -25,8 +30,8 @@ const UserDetailsFilter = ({ user }: UserProps) => {
       if (option === option2) setValue(sum);
       if (option === 12) setValue(sum);
 
-      setPeriod('período');
-    } else {    
+      setTextPeriod('período');
+    } else {
       swal(
         `${user.name} possui apenas ${user.parcel} parcelas`
       )
@@ -34,14 +39,30 @@ const UserDetailsFilter = ({ user }: UserProps) => {
   };
 
   const customCalculate = () => {
-    console.log('CLICOU');
+    const currentDate = H.currentDate();
+
+    const customDateWithFixedDay = H.dateWithFixedDay(customDate, +user.payday);
+    const currentDateWithFixedDay = H.dateWithFixedDay(currentDate, +user.payday);
+
+    const newPeriodValue = H.calculatesTheValueOfTheChosenPeriod(
+      customDateWithFixedDay,
+      user.parcel,
+      initialValue,
+      currentDateWithFixedDay
+    );
+
+    if (newPeriodValue) {
+      const formattedPeriodValue = H.formatToBRL(newPeriodValue);
+
+      setPeriodValue(formattedPeriodValue);
+    }
   };
 
   return (
     <C.Container>
 
       <C.Message>
-        {`Neste ${period} você receberá ${totalValueOfMonth}`}
+        {`Neste ${textPeriod} você receberá ${periodValue}`}
       </C.Message>
 
       <C.P>Verifique outros períodos:</C.P>
@@ -55,7 +76,7 @@ const UserDetailsFilter = ({ user }: UserProps) => {
             <C.Border onClick={() => calculate(option1)}>
               <C.Number>
                 {option1}
-              </C.Number>    
+              </C.Number>
             </C.Border>
 
             <C.Data>meses</C.Data>
@@ -67,7 +88,7 @@ const UserDetailsFilter = ({ user }: UserProps) => {
             <C.Border onClick={() => calculate(option2)}>
               <C.Number>
                 {option2}
-              </C.Number>            
+              </C.Number>
             </C.Border>
 
             <C.Data>meses</C.Data>
@@ -79,7 +100,7 @@ const UserDetailsFilter = ({ user }: UserProps) => {
             <C.Border onClick={() => calculate(option3)}>
               <C.Number>
                 {option3}
-              </C.Number>            
+              </C.Number>
             </C.Border>
 
             <C.Data>ano</C.Data>
@@ -88,8 +109,44 @@ const UserDetailsFilter = ({ user }: UserProps) => {
 
         </C.Filters>
 
-        <C.CustomFilter onClick={() => customCalculate()}>
-          Escolha um outro período
+        <C.P>Ou escolha um período personalizado
+  
+          <C.IconQuestion>
+
+            <Icon
+              icon='ant-design:question-circle-filled'
+              onClick={() => swal(
+                'Período personalizado',
+                `
+                  Define você mesmo uma data e clique no circulo de "check" para calcularmos o período definido
+
+                  Obs: O dia será ajustado para o mesmo da data de vencimento da parcela.
+                `
+              )}
+            />
+
+          </C.IconQuestion>
+
+        </C.P>
+
+        <C.CustomFilter>
+
+          <C.Calendar
+            name='start-date'
+            type='date'
+            onChange={({ target: { value }}) => setCustomDate(value)}
+            value={customDate}
+          />
+
+          <C.IconCheck>
+
+            <Icon
+              icon="ci:circle-check-outline"
+              onClick={() => customCalculate()}
+            />
+
+          </C.IconCheck>
+
         </C.CustomFilter>
 
       </C.FilterContainer>
